e025a927de85ea2a05ec3eabb9e4d3adbcb15f81
Brian Arthur <tricpony@mac.com>
2021-05-15 20:24:20 -0400

    Initial Commit

---
+++ NikeLab/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NikeLab
+//
+//  Created by aarthur on 5/15/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
