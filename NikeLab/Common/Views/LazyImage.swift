e52831b533bcab3e258407008407cf3e5979d0e6
Brian Arthur <tricpony@mac.com>
2021-05-19 03:58:49 -0400

    added performImageFetch

--- NikeLab/Common/Views/LazyImage.swift
+++ NikeLab/Common/Views/LazyImage.swift
@@ -18,28 +18,31 @@ struct LazyImage: View {
             image?.resizable()
 
             ProgressView().opacity(loading ? 1 : 0)
-        }.onAppear(perform: {
-            let url = URL(string: imageUrlString)
-            Loader<Data>().loadData(url) { result in
-                DispatchQueue.main.async {
-                    loading = false
-                    switch result {
-                    case .success(let data):
-                        guard let data = data else {
-                            image = Image("NoArt")
-                            return
-                        }
-                        guard let uiImage = UIImage(data: data) else {
-                            image = Image("NoArt")
-                            return
-                        }
-                        image = Image(uiImage: uiImage)
-                    case .failure( _ ):
+        }.onAppear(perform: performImageFetch)
+    }
+    
+    /// Fetch and initialize image property.
+    private func performImageFetch() {
+        let url = URL(string: imageUrlString)
+        Loader<Data>().loadData(url) { result in
+            DispatchQueue.main.async {
+                loading = false
+                switch result {
+                case .success(let data):
+                    guard let data = data else {
                         image = Image("NoArt")
+                        return
                     }
+                    guard let uiImage = UIImage(data: data) else {
+                        image = Image("NoArt")
+                        return
+                    }
+                    image = Image(uiImage: uiImage)
+                case .failure( _ ):
+                    image = Image("NoArt")
                 }
             }
-        })
+        }
     }
 }
 
